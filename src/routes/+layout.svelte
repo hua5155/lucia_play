081cbb88fe1b0a9d844cc0cd4dc79f5eb2617212
hua5155 <s17115155@stu.edu.tw>
2023-06-14 10:28:19 +0800

    Protected route prototype.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 
 <main class="h-fit min-h-screen w-screen max-w-full bg-black px-[5%] py-[3%] text-xl text-white">
 	<nav class="fixed left-0 top-0 flex h-10 w-full items-center justify-center space-x-5">
+		<a href="/user1">User1's secret</a>
+		<a href="/user2">User2's secret</a>
 		<a href="/">Home</a>
 		<a href="/login">Login</a>
 		<a href="/register">Sign up</a>
